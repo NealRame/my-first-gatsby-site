489a79ac740c5bb3396fa0eb2ca2d2b9a9547495
Julien Graziano <julien@graziano.fr>
2022-06-11 01:25:08 +0200

    Finished Gatsby Tutorial Part 2

---
+++ src/components/layout.tsx
@@ -0,0 +1,33 @@
+import { Link } from "gatsby"
+import * as React from "react"
+
+import {
+    container,
+    heading,
+    navLinks,
+    navLinkItem,
+    navLinkText,
+} from "./layout.module.css"
+
+export interface ILayoutData {
+    pageTitle: string
+    children: React.ReactNode
+}
+
+const Layout = ({ pageTitle, children }: ILayoutData) => (
+    <div className={ container }>
+        <title>{ pageTitle }</title>
+        <nav>
+            <ul className={ navLinks }>
+                <li className={ navLinkItem }><Link className={ navLinkText } to="/">Home</Link></li>
+                <li><Link to="/about/">About</Link></li>
+            </ul>
+        </nav>
+        <main>
+            <h1 className={ heading }>{ pageTitle }</h1>
+            { children }
+        </main>
+    </div>
+)
+
+export default Layout
---
+++ src/pages/about.tsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+
+const AboutPage = () => (
+    <Layout pageTitle="About Me">
+        <p>Hi there! I'm a software engineer who is passionate about building web applications.</p>
+    </Layout>
+)
+
+export default AboutPage
