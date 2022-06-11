03a2f84c75d4e86765f15136a53b0d09065b74d1
Julien Graziano <julien@graziano.fr>
2022-06-11 14:01:43 +0200

    Indent

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,9 @@ import * as React from "react"
 import Layout from "../components/layout"
 
 const IndexPage = () => (
-  <Layout pageTitle="Home Page">
-    <p>I'm making this by following the Gatsby Tutorial.</p>
-  </Layout>
+    <Layout pageTitle="Home Page">
+        <p>I'm making this by following the Gatsby Tutorial.</p>
+    </Layout>
 )
 
 export default IndexPage
