f0e44fa5a2d1f282f7f90a57d2ec226bbdcf3c3a
Julien Graziano <julien@graziano.fr>
2022-06-11 14:50:29 +0200

    Finished Gatsby Tutorial Part 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { StaticImage } from "gatsby-plugin-image"
 import * as React from "react"
 
 import Layout from "../components/layout"
@@ -5,6 +6,10 @@ import Layout from "../components/layout"
 const IndexPage = () => (
     <Layout pageTitle="Home Page">
         <p>I'm making this by following the Gatsby Tutorial.</p>
+        <StaticImage
+            src="../images/ace.jpg"
+            alt="Random Image"
+        />
     </Layout>
 )
 
