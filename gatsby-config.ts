be1ac97d185823302f695d4143e08bb56549270e
Julien Graziano <julien@graziano.fr>
2022-06-22 16:30:11 +0200

    Add Gatsby Sass plugins

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -6,6 +6,7 @@ const config: GatsbyConfig = {
   },
   plugins: [
     "gatsby-plugin-image",
+    "gatsby-plugin-sass",
     "gatsby-plugin-sharp",
     {
       resolve: "gatsby-source-filesystem",
