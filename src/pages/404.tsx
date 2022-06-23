9262ee175d64d7ecb4903b2d93590f6d1021653b
Julien Graziano <julien@graziano.fr>
2022-06-23 11:14:00 +0200

    Update 404 page layout

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,6 +1,8 @@
 import * as React from "react"
 import { Link } from "gatsby"
 
+import Layout from "../components/layout"
+
 // styles
 const pageStyles = {
   color: "#232129",
@@ -24,31 +26,11 @@ const codeStyles = {
   borderRadius: 4,
 }
 
-// markup
 const NotFoundPage = () => {
-  return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
+    return (
+        <Layout pageTitle="404 Not Found">
+        </Layout>
+    )
 }
 
 export default NotFoundPage
