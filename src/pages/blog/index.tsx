d85981284794893d6c55f2c26d11a4d209d672e6
Julien Graziano <julien@graziano.fr>
2022-07-04 00:19:57 +0200

    Indent

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -5,14 +5,12 @@ import Layout from "../../components/layout"
 
 const BlogPage = ({ data }: IBlogPostsQueryData) => (
     <Layout pageTitle="My Blog Posts">
-        {
-            data.allMdx.nodes.map(node => (
-                <article key={ node.id }>
-                    <h2><Link to={ `/blog/${node.slug}` }>{ node.frontmatter.title }</Link></h2>
-                    <p>Posted: { node.frontmatter.date }</p>
-                </article>
-            ))
-        }
+        {data.allMdx.nodes.map(node => (
+            <article key={ node.id }>
+                <h2><Link to={ `/blog/${node.slug}` }>{ node.frontmatter.title }</Link></h2>
+                <p>Posted: { node.frontmatter.date }</p>
+            </article>
+        ))}
     </Layout>
 )
 
