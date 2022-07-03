8df0fa5ee7ccd433b6e45abcb36cbfe92912f475
Julien Graziano <julien@graziano.fr>
2022-07-04 00:20:05 +0200

    Add page title

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,6 +4,7 @@ import Layout from "../components/layout"
 
 const AboutPage = () => (
     <Layout pageTitle="About Me">
+        <h1>About me</h1>
         <p>Hi there! I'm a software engineer who is passionate about building web applications.</p>
     </Layout>
 )
