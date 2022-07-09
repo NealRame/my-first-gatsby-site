f64e55463d312b8114e11c8b1de86df7a6729d5a
Julien Graziano <julien@graziano.fr>
2022-07-09 22:53:05 +0200

    Indent

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -7,25 +7,21 @@ const AboutPage = () => (
         <h1>About Me</h1>
         <p>
             Hi there! I'm a software developer based in <i><a href="https://goo.gl/maps/dDUwS3yW2rxizpD19">Lille</a></i>, France.
-        </p>
-        <p>
+        </p><p>
             I am currently employed by the company <i>Arobas Music</i> where I participate in the development of the <i><a href="https://www.guitar-pro.com">Guitar Pro</a></i> application.
-        </p>
-        <p>
+        </p><p>
             My main hobby is coding.
-        </p>
-        <p>
-            I love creating web applications and I'm always looking for new things to learn to improve my knowledge and skills. You will find on my site, some of creations.
-        </p>
-        <p>
+        </p><p>
+            I love creating web applications and I'm always looking for new things to learn to improve my knowledge and skills.
+        </p><p>
+            You will find on my site, some of creations.
+        </p><p>
             My favorite languages are (in alphabetical order):
-            <ul>
-                <li>C++,</li>
-                <li>Python,</li>
-                <li>Typescript.</li>
-            </ul>
-        </p>
-        <p>
+        </p><ul>
+            <li>C++,</li>
+            <li>Python,</li>
+            <li>Typescript.</li>
+        </ul><p>
             I also enjoy photography, 2D video games, electronic music and forest walks with my dog.
         </p>
     </Layout>
