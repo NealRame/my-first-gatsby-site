4d7dbd15dcf1a6ef48c72a5424104f1328850e3d
Julien Graziano <julien@graziano.fr>
2022-07-09 22:41:58 +0200

    About Me page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,8 +4,30 @@ import Layout from "../components/layout"
 
 const AboutPage = () => (
     <Layout pageTitle="About Me">
-        <h1>About me</h1>
-        <p>Hi there! I'm a software engineer who is passionate about building web applications.</p>
+        <h1>About Me</h1>
+        <p>
+            Hi there! I'm a software developer based in <i><a href="https://goo.gl/maps/dDUwS3yW2rxizpD19">Lille</a></i>, France.
+        </p>
+        <p>
+            I am currently employed by the company <i>Arobas Music</i> where I participate in the development of the <i><a href="https://www.guitar-pro.com">Guitar Pro</a></i> application.
+        </p>
+        <p>
+            My main hobby is coding.
+        </p>
+        <p>
+            I love creating web applications and I'm always looking for new things to learn to improve my knowledge and skills. You will find on my site, some of creations.
+        </p>
+        <p>
+            My favorite languages are (in alphabetical order):
+            <ul>
+                <li>C++,</li>
+                <li>Python,</li>
+                <li>Typescript.</li>
+            </ul>
+        </p>
+        <p>
+            I also enjoy photography, 2D video games, electronic music and forest walks with my dog.
+        </p>
     </Layout>
 )
 
