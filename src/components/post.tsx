6472d1a5544cde24536143a1a5e36e0212f05f82
Julien Graziano <julien@graziano.fr>
2022-07-18 22:08:04 +0200

    Try to fix JSS

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -1,24 +1,13 @@
 import { Link } from "gatsby"
 
 import * as React from "react"
-import { createUseStyles } from "react-jss"
 
 type IPostProps = {
     post: IBlogPostEntryData
 }
 
-const useStyles = createUseStyles({
-    postEntry: {
-        h2: {
-            marginBottom: 0,
-            paddingBottom: 0,
-        },
-    },
-})
-
 const Post = ({ post }: IPostProps) => {
-    const classes = useStyles()
-    return <article className={ classes.postEntry } key={ post.id }>
+    return <article key={ post.id }>
         <h2>
             <Link to={ `/blog/${post.slug}` }>
                 { post.frontmatter.title }
--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -4,14 +4,30 @@ import * as React from "react"
 import Post from "../../components/post"
 import Layout from "../../components/layout"
 
-const BlogPage = ({ data }: IBlogPostsQueryData) => (
-    <Layout pageTitle="My Blog Posts">
+import { createUseStyles } from "react-jss"
+
+const useStyles = createUseStyles({
+    postEntry: {
+        h2: {
+            marginBottom: 0,
+            paddingBottom: 0,
+        },
+    },
+}, {
+    name: "BlogIndex",
+})
+
+const BlogPage = ({ data }: IBlogPostsQueryData) => {
+    const style = useStyles()
+    return <Layout pageTitle="My Blog Posts">
         <h1>Blog</h1>
         {data.allMdx.nodes.map(node => (
-            <Post post={ node } key={ node.id }/>
+            <div className={ style.postEntry } key={ node.id }>
+                <Post post={ node }/>
+            </div>
         ))}
     </Layout>
-)
+}
 
 export const query = graphql`
     query {
